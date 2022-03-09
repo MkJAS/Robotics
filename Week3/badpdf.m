4f0d066323adab23f2d023e57d15ec588f59a323
MkJAS <joseph_fanbase@hotmail.com>
2022-03-09 13:49:05 +1100

    done?

--- Week3/badpdf.m
+++ Week3/badpdf.m
@@ -7,16 +7,25 @@ J1 = Link('d',0.475,'a',0.18,'alpha',pi/2,'offset',0,'qlim',[-170*pi/180 170*pi/
 
 J2 = Link('d',0,'a',0.385,'alpha',0,'offset',pi/2,'qlim',[-90*pi/180 135*pi/180]);
 % 
-J3 = Link('d',0,'a',0.116,'alpha',-pi/2,'offset',0,'qlim',[-80*pi/180 165*pi/180]);
+J3 = Link('d',0,'a',0.1,'alpha',-pi/2,'offset',0,'qlim',[-80*pi/180 165*pi/180]);
 % 
-J4 = Link('d',0,'a',0.329,'alpha',0,'offset',0,'qlim',[-185*pi/180 185*pi/180]);
+J4 = Link('d',-0.329,'a',0,'alpha',pi/2,'offset',0,'qlim',[-185*pi/180 185*pi/180]);
 % 
-J5 = Link('d',0,'a',0.084,'alpha',-pi/2,'offset',0,'qlim',[-120*pi/180 120*pi/180]);
+J5 = Link('d',0,'a',0,'alpha',pi/2,'offset',0,'qlim',[-120*pi/180 120*pi/180]);
 % 
-J6 = Link('d',0,'a',0,'alpha',0,'offset',0,'qlim',[-360*pi/180 360*pi/180]);
+J6 = Link('d',0.085,'a',0,'alpha',0,'offset',0,'qlim',[-360*pi/180 360*pi/180]);
 
-robot = SerialLink([J1 J2 J3 J4]);
+robot = SerialLink([J1 J2 J3 J4 J5 J6]);
 
-q = zeros(1,4);
+q = zeros(1,6);
 
 robot.plot(q)
+
+robot.teach;
+
+%% 
+
+for i=1:6
+    
+    
+end
